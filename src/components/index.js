7ce6b0749423cbfac9c47f2a22dd4bdd8fcd975e
pbhatt009 <bhattpriyanshu49@gmail.com>
2025-04-05 12:34:47 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,47 +1,59 @@
+// Layout Components
 import Header from "./header/header";
 import Footer from "./footer/footer";
 import Container from "./container/container";
 import Logo from "./Logo";
 import Logoutbtn from "./header/logoutbtn";
+
+// UI Components
 import Input from "./input";
 import Button from "./button";
-import RTE from "./rte"
-import Login from "./login";
-import Signup from "./signup";
+import RTE from "./rte";
+import Select from "./select";
 import Postform from "./postform/postform";
 import Postcard from "./postcard";
-import Authlayout from "./authlayout";
-import LoginPage from "../pages/login"
+
+// Auth Components
+import Login from "./login";
+import Signup from "./signup";
+import Authlayout from "./Authlayout";
+
+// Pages
+import LoginPage from "../pages/login";
 import SignupPage from "../pages/signup";
 import Home from "../pages/home";
 import Allpost from "../pages/allpost";
 import Addpost from "../pages/addpost";
 import Editpost from "../pages/editpost";
 import Post from "../pages/post";
-import Select from "./select";
-
 
 export {
-    Header,
-    Footer,
-    Container,
-    Logo,
-    Logoutbtn,
-    Input,
-    Button,
-    RTE,
-    Login,
-    Signup,
-    Postform,
-    Postcard,
-    Authlayout,
-    LoginPage,
-    SignupPage,
-    Home,
-    Allpost,
-    Addpost,
-    Editpost,
-    Post,
-    Select,
-    
+  // Layout
+  Header,
+  Footer,
+  Container,
+  Logo,
+  Logoutbtn,
+
+  // UI
+  Input,
+  Button,
+  RTE,
+  Select,
+  Postform,
+  Postcard,
+
+  // Auth
+  Login,
+  Signup,
+  Authlayout,
+
+  // Pages
+  LoginPage,
+  SignupPage,
+  Home,
+  Allpost,
+  Addpost,
+  Editpost,
+  Post,
 };
